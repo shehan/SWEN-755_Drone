4cf616f3abc3654f2ee6eed74cdfd01bd1b61009
Nasir Safdari <nxs5899@rit.edu>
2017-09-28 17:13:55 -0600

    mission planning non-critical

--- SWEN-755_Drone/Modules/Non-Critical/MissionPlanning/Program.cs
+++ SWEN-755_Drone/Modules/Non-Critical/MissionPlanning/Program.cs
@@ -1,15 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+using Common;
 
 namespace MissionPlanning
 {
-    class Program
+    class Program: Heartbeat
+
     {
+        private static Program _p;
         static void Main(string[] args)
         {
+            _p = new Program();
+
+            if (args != null)
+            {
+                _p.StartBeating(args[0], "MissionPlanning", ModuleType.NonCritical);
+            }
+            else
+            {
+                Console.WriteLine("The program cannot start without the required arguments");
+                Console.ReadLine();
+                return;
+            }
+
+            var crashTimer = new Timer { Interval = 10000 };
+            crashTimer.Elapsed += CrashTimer_Elapsed;
+            crashTimer.Enabled = true;
+
+            var workTimer = new Timer { Interval = 3000 };
+            workTimer.Elapsed += WorkTimer_Elapsed;
+            workTimer.Enabled = true;
+
+            Console.ReadLine();
+        }
+        private static void WorkTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _p.WorkBeat();
+        }
+        private static void CrashTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                var random = new Random();
+                var randomNumber = random.Next(0, 3);
+                randomNumber = 100 / randomNumber;
+            }
+            catch (Exception ex)
+            {
+                Process.GetCurrentProcess().Kill();
+            }
         }
     }
 }
